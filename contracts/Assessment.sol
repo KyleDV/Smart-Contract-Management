faec744677df91ffe871f00e5d7a3b8474d7200f
KyleDV <113366907+KyleDV@users.noreply.github.com>
2023-11-20 02:00:50 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -9,6 +9,8 @@ contract Assessment {
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event Transfer(string _address, uint256 amount);
+    event PayBills(uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
@@ -43,4 +45,45 @@ contract Assessment {
 
         emit Withdraw(_withdrawAmount);
     }
+
+    function transferamt(string memory to, uint256 _transferAmount) public {
+
+        uint _previousBalance = balance;
+
+        require(balance >= _transferAmount, "Not Enough Balance");
+        
+        balance -= _transferAmount;
+
+        if(balance != (_previousBalance - _transferAmount))
+            revert();
+        
+        emit Transfer(to, _transferAmount);
+    }
+
+    function paybills(int billno, uint256 months) public {
+
+        uint _previousBalance = balance;
+
+        uint256 amount = 0;
+
+        if(billno == 1){
+            amount = 20;
+        }else if(billno == 2){
+            amount = 15;
+        }else{
+            amount = 0;
+        }
+
+        amount = amount * months;
+
+        require(balance >= amount, "Not Enough Balance");
+        
+        balance -= amount;
+
+        if(balance != (_previousBalance - amount))
+            revert();
+
+        emit PayBills(amount);
+    }
+
 }
